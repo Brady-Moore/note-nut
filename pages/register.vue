2bc06e4fae94141222ddd18c8484e9a15b8b4cc4
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2024-12-18 16:34:00 +0900

    inputs and sign up button template

--- pages/register.vue
+++ pages/register.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="flex bg-black h-screen">
     <!-- Side Bar -->
-    <div class="bg-zinc-900 w-[516px] p-7">
+    <div class="bg-zinc-900 w-[516px] p-10 flex flex-col justify-center">
       <Logo />
-      <h1>Sign up for free!</h1>
+      <h1 class="text-white font-bold text-lg mt-7">Sign up for free!</h1>
+      <p class="text-zinc-300 text-sm">Have an account already? <span class="font-bold text-yellow-500 underline">Log in</span></p>
+      <!-- Login Form -->
+      <div class="mt-10">
+        <label
+          for="email"
+          class="text-zinc-300 text-sm block mb-1"
+          >Email Address</label
+        >
+        <input
+          type="email"
+          class="block w-full bg-gray-700 border border-zinc-400 rounded text-white text-sm px-3 py-2 placeholder:text-zinc-400"
+          placeholder="email@example.com" />
+      </div>
+      <div>
+        <label
+          for="password"
+          class="text-zinc-300 text-sm block mb-1"
+          >Password</label
+        >
+        <input
+          type="password"
+          class="block w-full bg-gray-700 border border-zinc-400 rounded text-white text-sm px-3 py-2 placeholder:text-zinc-400"
+          placeholder="***********" />
+      </div>
+      <!-- Sign Up Button -->
+      <div>
+        <button class="w-full mt-4 bg-yellow-500 rounded-full px-3 py-2 text-sm font-bold">Sign Up</button>
+      </div>
     </div>
     <!-- Note Demo -->
     <div></div>
