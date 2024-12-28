7b4e4877b6b14f5820dd7b9dc8b7a9046935b066
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2024-12-28 10:55:00 +0900

    add submit button

--- pages/register.vue
+++ pages/register.vue
@@ -6,34 +6,47 @@
       <h1 class="text-white font-bold text-lg mt-7">Sign up for free!</h1>
       <p class="text-zinc-300 text-sm">Have an account already? <span class="font-bold text-yellow-500 underline">Log in</span></p>
       <!-- Login Form -->
-      <div class="mt-10">
-        <label
-          for="email"
-          class="text-zinc-300 text-sm block mb-1"
-          >Email Address</label
-        >
-        <input
-          type="email"
-          class="block w-full bg-gray-700 border border-zinc-400 rounded text-white text-sm px-3 py-2 placeholder:text-zinc-400"
-          placeholder="email@example.com" />
-      </div>
-      <div>
-        <label
-          for="password"
-          class="text-zinc-300 text-sm block mb-1"
-          >Password</label
-        >
-        <input
-          type="password"
-          class="block w-full bg-gray-700 border border-zinc-400 rounded text-white text-sm px-3 py-2 placeholder:text-zinc-400"
-          placeholder="***********" />
-      </div>
-      <!-- Sign Up Button -->
-      <div>
-        <button class="w-full mt-4 bg-yellow-500 rounded-full px-3 py-2 text-sm font-bold">Sign Up</button>
-      </div>
+      <form @submit.prevent="submit">
+        <div class="mt-10">
+          <label
+            for="email"
+            class="text-zinc-300 text-sm block mb-1"
+            >Email Address</label
+          >
+          <input
+            v-model="email"
+            type="email"
+            class="block w-full bg-gray-700 border border-zinc-400 rounded text-white text-sm px-3 py-2 placeholder:text-zinc-400"
+            placeholder="email@example.com" />
+        </div>
+        <div>
+          <label
+            for="password"
+            class="text-zinc-300 text-sm block mb-1"
+            >Password</label
+          >
+          <input
+            v-model="password"
+            type="password"
+            class="block w-full bg-gray-700 border border-zinc-400 rounded text-white text-sm px-3 py-2 placeholder:text-zinc-400"
+            placeholder="***********" />
+        </div>
+        <!-- Sign Up Button -->
+        <div>
+          <button class="w-full mt-4 bg-yellow-500 rounded-full px-3 py-2 text-sm font-bold">Sign Up</button>
+        </div>
+      </form>
     </div>
     <!-- Note Demo -->
     <div></div>
   </div>
 </template>
+
+<script setup>
+const email = ref("");
+const password = ref("");
+
+function submit() {
+  console.log("submit");
+}
+</script>
