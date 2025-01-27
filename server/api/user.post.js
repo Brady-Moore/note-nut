8862e8b511c598f3ac2132d50cd2f1d607d5cb6e
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-01-27 17:02:00 +0900

    added backend email/password validation

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,10 +1,24 @@
 // /api/user POST
 import bcrypt from "bcryptjs";
+import validator from "validator";
 import { prisma } from "../utils/prisma.js";
 
 export default defineEventHandler(async (event) => {
   try {
     const body = await readBody(event);
+
+    if (!validator.isEmail(body.email)) {
+      throw createError({
+        statusCode: 400,
+        message: "Invalid email address",
+      });
+    }
+    if (!validator.isStrongPassword(body.password)) {
+      throw createError({
+        statusCode: 400,
+        message: "Password must be at least 8 characters long and contain a lowercase letter, an uppercase letter, a number, and a symbol.",
+      });
+    }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(body.password, salt);
