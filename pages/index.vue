be536d664aa9840c63a847bc7057fa8499be15a9
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-01-29 18:31:00 +0900

    selected notes and todays

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,12 @@
       <!-- Today -->
       <div>
         <p class="text-xs font-bold text-zinc-300 mt-10 mb-3">Today</p>
-        <div class="pl-2">
+        <div class="pl-2 space-y-2">
           <div
             v-for="note in notes"
-            class="p-2 bg-yellow-500 rounded-lg">
+            class="p-2 bg-yellow-500 rounded-lg cursor-pointer"
+            :class="{ 'bg-yellow-500': note.id === selectedNote.id, 'hover:bg-yellow-500/20': note.id !== selectedNote.id }"
+            @click="selectedNote = note">
             <h3 class="text-sm font-bold text-white truncate">{{ note.text.substring(0, 40) }}</h3>
             <div class="leading-none truncate text-zinc-400">
               <span class="text-xs text-white mr-4">{{
@@ -62,12 +64,11 @@
         </button>
       </div>
       <div class="max-w-[450px] mx-auto">
-        <p class="text-zinc-300">February 23rd, 2089</p>
-        <p class="text-zinc-100 my-3">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci debitis quas sit rem dolore tenetur pariatur cupiditate corporis blanditiis
-          architecto, iure quod alias provident soluta maxime! Voluptatibus incidunt corrupti corporis?
+        <p class="text-zinc-300 font-playfair">{{ new Date(selectedNote.updatedAt).toLocaleDateString() }}</p>
+        <p class="text-zinc-100 my-3 font-playfair">
+          {{ selectedNote.text }}
         </p>
-        <p class="text-zinc-100 my-3">
+        <p class="text-zinc-100 my-3 font-playfair">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci debitis quas sit rem dolore tenetur pariatur cupiditate corporis blanditiis
           architecto, iure quod alias provident soluta maxime! Voluptatibus incidunt corrupti corporis?
         </p>
@@ -78,11 +79,23 @@
 
 <script setup>
 const notes = ref([]);
+const selectedNote = ref({});
 definePageMeta({
   middleware: ["auth"],
 });
 
+const todaysNotes = computed(() => {
+  return notes.value.filter((note) => {
+    const noteDate = new Date(note.updatedAt);
+    return noteDate.toDateString() === new Date().toDateString();
+  });
+});
+
 onMounted(async () => {
   notes.value = await $fetch("/api/notes");
+
+  if (notes.value.length > 0) {
+    selectedNote.value = notes.value[0];
+  }
 });
 </script>
