b5bc9bb0bda4f581be45f678811b5d833be0a9e0
Brady Moore <140730015+Brady-Moore@users.noreply.github.com>
2025-01-29 21:44:00 +0900

    user notes view token integration

--- server/api/notes.js
+++ server/api/notes.js
@@ -1,6 +1,22 @@
+import jwt from "jsonwebtoken";
+
 export default defineEventHandler(async (event) => {
   try {
-    const user = await prisma.note.findMany();
+    const cookies = parseCookies(event);
+    const token = cookies.NoteNestJWT;
+    if (!token) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: "Not authorized to access notes",
+      });
+    }
+
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+    const user = await prisma.note.findMany({
+      where: {
+        userId: decodedToken.id,
+      },
+    });
     return notes;
   } catch (error) {}
 });
